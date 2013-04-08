8796340e464bd965850bf558c9ee4111123b0af9
Etienne Laurin <atnnn@rethinkdb.com>
2013-04-08 14:14:46 -0700

    stash

--- Database/RethinkDB/MapReduce.hs
+++ Database/RethinkDB/MapReduce.hs
@@ -2,38 +2,49 @@ module Database.RethinkDB.MapReduce where
 
 import Control.Monad.State
 
+import Database.RethinkDB.Protobuf.Ql2.Term2.TermType
+
 import Database.RethinkDB.Term
 
 termToMapReduce :: (Term -> Term) -> State QuerySettings (Term, Term, Term)
 termToMapReduce = undefined
 
-{-
-data MapReduce = MapReduce {
-    termSequence :: Term T.Datum,
-    termMap :: Maybe F1,
-    termBase :: Maybe (Term T.Datum),
-    termReduce :: Maybe F2,
-    termFinalize :: Maybe F1 }
+data MapReduce =
+    None Term |
+    Map [Term] |
+    MapReduce [Term] Term |
+    MapReduceFinal [Term] Term [Term]
 
-tvSequence :: Term t -> Maybe (State QuerySettings BaseTerm)
-tvSequence (Term s Nothing Nothing Nothing Nothing) = Just s
-tvSequence _ = Nothing
+toReduce :: MapReduce -> MapReduce
+toReduce (Map x) = MapReduce x idt
+toReduce mr = mr
 
-tvFinalize :: Term t -> Maybe (State QuerySettings BaseTerm, Maybe F1)
-tvFinalize (Term s Nothing Nothing Nothing f) = Just (s, f)
-tvFinalize _ = Nothing
+idt :: Term
+idt = Term $ do
+        v <- newVar
+        baseTerm $ op FUNC ([v], v) ()
 
-tvMap :: Term t -> Maybe (State QuerySettings BaseTerm, Maybe F1, Maybe F1)
-tvMap (Term s m Nothing Nothing f) = Just (s, m, f)
-tvMap _ = Nothing
+sameVar = undefined
 
-tvReduce :: Term t -> Maybe (State QuerySettings BaseTerm, Maybe (Term T.Datum),
-                             Maybe F2, Maybe F1)
-tvReduce (Term s Nothing b r f) = Just (s, b, r, f)
-tvReduce _ = Nothing
+notNone :: MapReduce -> Bool
+notNone None{} = False
+notNone _ = True
 
-tvMapReduce :: Term t -> Maybe (State QuerySettings BaseTerm, Maybe F1,
-                                Maybe (Term T.Datum), Maybe F2, Maybe F1)
-tvMapReduce (Term s m b r f) = Just (s, m, b, r, f)
-tvMapReduce _ = Nothing
--}
+wrap :: BaseTerm -> Term
+wrap = Term . return
+
+toMapReduce :: BaseTerm -> BaseTerm -> MapReduce
+toMapReduce _ t@(BaseTerm DATUM _ _ _) = None $ wrap t
+toMapReduce v t@(BaseTerm VAR _ w _) | sameVar v w = Map []
+toMapReduce v t@(BaseTerm type' _ args optargs) = let
+    args' = map (toMapReduce v) args
+    optargs' = map (\(BaseAttribute k vv) -> (k, toMapReduce v vv)) optargs
+    count = length $ filter notNone $ args' ++ map snd optargs'
+    rebuild = (if count == 1 then rebuild0 else rebuildx) type' args' optargs'
+  in if count == 0 then None $ wrap t else
+         case (type', args') of
+           _ -> undefined
+
+rebuild0 = undefined
+
+rebuildx = undefined
